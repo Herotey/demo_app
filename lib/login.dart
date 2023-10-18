6c555b853b045ebd5169658f8e69ab47212ad74e
duongherotey <nak.duongherotey@ecoinsoft.com>
2023-10-18 14:29:55 +0700

    apply shared preferences for testing counter value

---
+++ lib/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Login Page',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: const Center(child: Text('Plese logig account')),
+    );
+  }
+}
